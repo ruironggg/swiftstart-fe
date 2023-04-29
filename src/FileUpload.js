612c1a4c7882ac7b6025798bc20a06636870fc6b
ruironggg <105039474+ruironggg@users.noreply.github.com>
2023-04-29 14:33:12 -0400

    updated functionality of 21st century upload button

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,29 +1,33 @@
-import React, { useState, useRef } from 'react';
-import './FileUpload.css';
+import React, { useState, useRef } from "react";
+import "./FileUpload.css";
 
 const FileUpload = () => {
-  const [selectedFile, setSelectedFile] = useState('');
+  const [selectedFile, setSelectedFile] = useState("");
   const fileInputRef = useRef(null);
 
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
   const submitFile = (event) => {
-    console.log('submitFile occurs')
+    console.log("submitFile occurs");
 
     // Create a new FormData object and append the selected file
     const formData = new FormData();
-    formData.append('file', selectedFile);
-  
+    formData.append("file", selectedFile);
+
     // Send a fetch request to the server
-    fetch('http://localhost:5000/generate-documentation', {
-      method: 'POST',
-      body: formData
+    fetch("http://localhost:5000/generate-documentation", {
+      method: "POST",
+      body: formData,
     })
-      .then(response => response.text())
-      .then(data => {
+      .then((response) => response.text())
+      .then((data) => {
         console.log(data);
         // Do something with the response data
       })
-      .catch(error => console.error(error));
-  };  
+      .catch((error) => console.error(error));
+  };
 
   const handleDrop = (event) => {
     event.preventDefault();
@@ -43,7 +47,7 @@ const FileUpload = () => {
   const handleBrowseClick = () => {
     fileInputRef.current.click();
   };
-  
+
   return (
     <div className="file-upload">
       <div
@@ -56,7 +60,7 @@ const FileUpload = () => {
           type="file"
           multiple
           ref={fileInputRef}
-          // onChange={handleFileChange}
+          onChange={handleFileSelect}
         />
         <button className="upload-file__button" onClick={submitFile}>
           Upload file
@@ -72,7 +76,7 @@ const FileUpload = () => {
         </div>
       )}
     </div>
-  );  
+  );
 };
 
 export default FileUpload;
