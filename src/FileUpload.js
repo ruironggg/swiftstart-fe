e3ac32f43bad01b2e9b2cbb41e3f9944caad2e8c
seancze <60085582+seancze@users.noreply.github.com>
2023-04-29 20:18:14 -0400

    refactor: remove redundant code

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -48,7 +48,6 @@ const FileUpload = () => {
         className={`flex items-center justify-center w-full`}
       >
         <label
-          for="dropzone-file"
           className={`${
             isDragActive ? "bg-gray-100" : "bg-white"
           } flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600`}
@@ -84,7 +83,6 @@ const FileUpload = () => {
               SVG, PNG, JPG or GIF (MAX. 800x400px)
             </p>
           </div>
-          <input id="dropzone-file" type="file" className="hidden" />
         </label>
       </div>
       <button
