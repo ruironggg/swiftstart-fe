2cd4684f3c80eddb4f63b957e15bde22f024c0f8
seancze <60085582+seancze@users.noreply.github.com>
2023-04-29 20:12:36 -0400

    feat: update file upload frontend + connect to github backend

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          File Upload App
+        <h2 className="text-center text-3xl font-extrabold text-gray-900">
+          SwiftStart
         </h2>
         <FileUpload />
-        <div className="mt-6 flex justify-center">
+        {/* Or Divider */}
+        <div className="flex items-center px-2">
+          <div className="flex-1 border-b-2 border-dashed border-gray-400"></div>
+          <span className="px-2 text-gray-400">Or</span>
+          <div className="flex-1 border-b-2 border-dashed border-gray-400"></div>
+        </div>
+        <div className="flex justify-center">
           <GithubSignIn />
         </div>
       </div>
--- src/FileUpload.js
+++ src/FileUpload.js
@@ -45,20 +45,47 @@ const FileUpload = () => {
     <div className="flex flex-col items-center">
       <div
         {...getRootProps()}
-        className={`w-full h-64 flex flex-col items-center justify-center border-4 border-dashed border-gray-400 rounded-lg ${
-          isDragActive ? "bg-gray-100" : "bg-white"
-        }`}
+        className={`flex items-center justify-center w-full`}
       >
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p className="text-gray-600 font-semibold">
-            Drop the .zip file here...
-          </p>
-        ) : (
-          <p className="text-gray-600 font-semibold">
-            Drag 'n' drop a .zip file here, or click to select a file
-          </p>
-        )}
+        <label
+          for="dropzone-file"
+          className={`${
+            isDragActive ? "bg-gray-100" : "bg-white"
+          } flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600`}
+        >
+          <input {...getInputProps()} />
+          <div className="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg
+              aria-hidden="true"
+              className="w-10 h-10 mb-3 text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+              ></path>
+            </svg>
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+              {isDragActive ? (
+                "Drop the .zip file here..."
+              ) : (
+                <>
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </>
+              )}
+            </p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              SVG, PNG, JPG or GIF (MAX. 800x400px)
+            </p>
+          </div>
+          <input id="dropzone-file" type="file" className="hidden" />
+        </label>
       </div>
       <button
         onClick={handleUpload}
